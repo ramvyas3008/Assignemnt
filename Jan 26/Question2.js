4b103fb847ea0f3c18f1e620f7afc37105a76186
ramvyas3008 <96729068+ramvyas3008@users.noreply.github.com>
2022-02-01 13:10:31 +0530

    Create Question2.js

---
+++ Jan 26/Question2.js
@@ -0,0 +1,10 @@
+var char = prompt("Enter to check wheather the number is vowel or consonat");
+
+if(char == "a" || char == "e" || char == "i" || char == "o" || char == "u")
+{
+	alert("Vowel");
+}
+else
+{
+	alert("Consonat");
+}
