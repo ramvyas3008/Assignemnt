31784ff69fc76771c671e8995e738f0713a8410d
ramvyas3008 <96729068+ramvyas3008@users.noreply.github.com>
2022-02-01 13:08:10 +0530

    Create Question1.js

---
+++ Jan 26/Question1.js
@@ -0,0 +1,20 @@
+var n1 = prompt("Enter the First Number");
+var n2 = prompt("Enter the Second Number");
+var n3 = prompt("Enter the Third Number");
+
+n1 = parseInt(n1);
+n2 = parseInt(n2);
+n3 = parseInt(n3);
+
+if (n1 < n2)
+{
+	alert(n1);
+
+}
+else if (n2 < n3)
+{
+	alert(n2);
+}
+else {
+	alert(n3);
+}
