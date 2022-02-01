f181cf7211502f849d8e2b2f92bf95a1d6fbd4da
ramvyas3008 <96729068+ramvyas3008@users.noreply.github.com>
2022-02-01 13:12:56 +0530

    Update Assignment 1.js

--- Assignment 1.js
+++ Assignment 1.js
@@ -1,7 +1,13 @@
 var a =18
 if (a>10)
-{console.log("a is greater than 10");}
+{
+  console.log("a is greater than 10");
+}
 else if (a==10)
-{console.log("a is equal to 10");}
+{
+  console.log("a is equal to 10");
+}
 else
-{console.log("a is not equal to 10");}
+{
+  console.log("a is not equal to 10");
+}
