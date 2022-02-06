0e4248a579313501efcd0daa3c8762db7470464a
ramvyas3008 <96729068+ramvyas3008@users.noreply.github.com>
2022-02-06 10:25:03 +0530

    Create fibonacci.js

---
+++ Jan 28/fibonacci.js
@@ -0,0 +1,15 @@
+var m=prompt("Enter n numbers");
+m=parseInt(m);
+var a=0;
+var b=1;
+ console.log(a);
+var num=10;
+for(var i=1;i<=num;i++)
+	{
+		
+		console.log(b);
+		var c=a+b;
+		a=b;
+		b=c;
+		
+	}
